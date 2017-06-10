0547699bc58f319b441e41c33d1516ea4e1e6027
Sharad Shinde <shindesharad71@gmail.com>
2017-06-10 14:27:41 +0530

    single post and all post fetching added

--- app/api/post.php
+++ app/api/post.php
@@ -6,5 +6,42 @@ $app = new \Slim\App;
 
 // Get All Posts
 $app->get('/api/posts', function(Request $request, Response $response){
-    echo 'posts';
+    $sql = "SELECT * FROM posts";
+
+    try{
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $posts = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($posts);
+
+    } catch(PDOEception $e){
+        echo '{"error": {"text": '.$e->getMessage().'}}';
+    }
+});
+
+// Get Single Post
+$app->get('/api/post/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM posts WHERE id=$id";
+    try{
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $post = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($post);
+
+    } catch(PDOEception $e){
+        echo '{"error": {"text": '.$e->getMessage().'}}';
+    }
+
 });
