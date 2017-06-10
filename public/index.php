54010a50e2f81361ec6560e67cb41dd6c87696d0
Sharad Shinde <shindesharad71@gmail.com>
2017-06-10 12:58:08 +0530

    selector according to url

--- public/index.php
+++ public/index.php
@@ -6,7 +6,27 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
-require_once('../app/api/post.php');
-require_once('../app/api/category.php');
+// Get Page Name From URL
+$_SERVER['REQUEST_URI_PATH'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$segments = explode('/', $_SERVER['REQUEST_URI_PATH']);
+if(isset($segments[2])){
+    $page_name = $segments[2];
+
+    if($page_name == 'posts' || $page_name == 'post')
+    {
+        require_once('../app/api/post.php');
+    }
+    elseif($page_name == 'categories' || $page_name == 'category')
+    {
+        require_once('../app/api/category.php');
+    }
+    else
+    {
+        die('Please Use API Endpoints');
+    }
+
+}
+
+
 
 $app->run();
