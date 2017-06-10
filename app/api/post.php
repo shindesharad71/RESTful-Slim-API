e93890baa3246edac3c77ab83ccf8840b7c1b167
Sharad Shinde <shindesharad71@gmail.com>
2017-06-10 12:32:32 +0530

    code init

---
+++ app/api/post.php
@@ -0,0 +1,9 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+$app->get('/api/posts', function(Request $request, Response $response){
+    echo 'posts';
+});
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+require_once('../app/api/post.php');
+
+$app->run();
